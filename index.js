618d88e92dc2668a80e979a39d27dcb254788e65
Owen Davies <owen@odavi.es>
2015-10-17 08:27:43 +0100

    Add support for passing in a config file path to index.js
    Also:
    - Remove some commented out code
    - Spacing and styling improvements
    - Add some more logging

--- index.js
+++ index.js
@@ -2,55 +2,57 @@
  * Created by austin on 9/18/14.
  */
 
-var fs = require('fs')
+module.exports = function(configPath) {
+  var         fs = require('fs'),
+            util = require('./lib/util'),
+           oauth = require('./lib/oauth'),
+         athlete = require('./lib/athlete'),
+        athletes = require('./lib/athletes'),
+      activities = require('./lib/activities'),
+           clubs = require('./lib/clubs'),
+            gear = require('./lib/gear'),
+        segments = require('./lib/segments'),
+  segmentEfforts = require('./lib/segmentEfforts'),
+         streams = require('./lib/streams'),
+         uploads = require('./lib/uploads');
 
-    , util = require('./lib/util')
-    , oauth = require('./lib/oauth')
-    , athlete = require('./lib/athlete')
-    , athletes = require('./lib/athletes')
-    , activities = require('./lib/activities')
-    , clubs = require('./lib/clubs')
-    , gear = require('./lib/gear')
-    , segments = require('./lib/segments')
-    , segmentEfforts = require('./lib/segmentEfforts')
-    , streams = require('./lib/streams')
-    , uploads = require('./lib/uploads')
-    ;
+  var strava = {};
+  if (!configPath) {
+    console.log("No configPath provided, using 'data/strava_config' instead");
+    configPath = "data/strava_config";
+  }
 
-var strava = {};
-var configPath = "data/strava_config";
-
-//attempt to grab the default config
-try {
+  try {
+    console.log("Reading config from " + configPath);
     var config = fs.readFileSync(configPath, {encoding: 'utf-8'});
     util.config = JSON.parse(config);
-} catch (err) {
-    //console.log(err)
-    console.log("no 'data/strava_config' file, continuing without...");
+  } catch (err) {
+    console.log("no config file, continuing without...");
+  }
+
+  if (process.env.STRAVA_ACCESS_TOKEN) {
+    util.config.access_token = process.env.STRAVA_ACCESS_TOKEN;
+  }
+  if (process.env.STRAVA_CLIENT_SECRET) {
+    util.config.client_secret = process.env.STRAVA_CLIENT_SECRET;
+  }
+  if (process.env.STRAVA_CLIENT_ID) {
+    util.config.client_id = process.env.STRAVA_CLIENT_ID;
+  }
+  if (process.env.STRAVA_REDIRECT_URI) {
+    util.config.redirect_uri = process.env.STRAVA_REDIRECT_URI;
+  }
+
+  strava.oauth          = oauth;
+  strava.athlete        = athlete;
+  strava.athletes       = athletes;
+  strava.activities     = activities;
+  strava.clubs          = clubs;
+  strava.gear           = gear;
+  strava.segments       = segments;
+  strava.segmentEfforts = segmentEfforts;
+  strava.streams        = streams;
+  strava.uploads        = uploads;
+
+  return strava;
 }
-
-//allow environment vars to override config vals
-if(process.env.STRAVA_ACCESS_TOKEN)
-  util.config.access_token = process.env.STRAVA_ACCESS_TOKEN;
-if(process.env.STRAVA_CLIENT_SECRET)
-  util.config.client_secret = process.env.STRAVA_CLIENT_SECRET;
-if(process.env.STRAVA_CLIENT_ID)
-  util.config.client_id = process.env.STRAVA_CLIENT_ID;
-if(process.env.STRAVA_REDIRECT_URI)
-  util.config.redirect_uri = process.env.STRAVA_REDIRECT_URI;
-
-//assign various api segments to strava object
-strava.oauth = oauth;
-strava.athlete = athlete;
-strava.athletes = athletes;
-strava.activities = activities;
-strava.clubs = clubs;
-strava.gear = gear;
-strava.segments = segments;
-strava.segmentEfforts = segmentEfforts;
-strava.streams = streams;
-strava.uploads = uploads;
-
-//and export
-module.exports = strava;
-
